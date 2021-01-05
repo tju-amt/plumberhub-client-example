b682e652ccae1ec0ab84891d17cd987b501a4b46
ChaosLee <chaos2lee@qq.com>
2021-01-05 19:13:38 +0800

    [FIXED] data can be stored

--- example/writeToEDF.py
+++ example/writeToEDF.py
@@ -5,23 +5,23 @@ import random
 import time
 from plumberhub import PlumberHubClient
 
-
 CHANNEL_NUMBER = 8
 
 edf_file = None
 
 def createEDF():
-    now = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
-    edf_file = pyedflib.EdfWriter(
-        r"recorder-" + now + r".edf", 1,
-        file_type = pyedflib.FILETYPE_BDFPLUS
-    )
+    global edf_file
+
+    now = time.strftime("%Y-%m-%d_%H_%M_%S", time.localtime())
+    filename = r"recorder-" + now + r".edf"
+    print("File: " + filename)
+    edf_file = pyedflib.EdfWriter(filename, CHANNEL_NUMBER, file_type = pyedflib.FILETYPE_BDFPLUS)
 
     channel_info_list = []
 
     for index in range(CHANNEL_NUMBER):
         channel_info_list.append({
-            'label': 'ch_' + index,
+            'label': 'ch_' + str(index),
             'dimension': 'μV',
             'sample_rate': 100,
             'physical_max': 100,
@@ -35,29 +35,34 @@ def createEDF():
     edf_file.setSignalHeaders(channel_info_list)
 
 def clearEDF():
-    if (edf_file !== None):
+    global edf_file
+
+    if (edf_file != None):
         edf_file.close()
-        edf_file === None
+        edf_file == None
 
 def handleSample(sample):
-    npSample = []
+    global edf_file
 
-    for value in sample.dataList:
-        npSample.append(np.array([value]))
+    if (edf_file != None):
+        npSample = []
 
-    if (edf_file !== None):
+        for value in sample.dataList:
+            npSample.append(np.array([value]))
         edf_file.writeSamples(npSample)
 
 client = PlumberHubClient(
     hostname = '127.0.0.1',
     port = 8080,
-    client_id = '',
+    client_id = '314dee1f82e82106c8ab4d51ee933c9a4c09209dfebc35b2f2f5fd55be73302e',
     onsample = handleSample
 )
 
 while True:
     if keyboard.is_pressed('space'):
-        createEDF()
-    elif keyboard.is_pressed('ctrl+space'):
+        if (edf_file == None):
+            createEDF()
+
+    elif keyboard.is_pressed('ctrl'):
+        print('Finished!')
         clearEDF()
-        break
